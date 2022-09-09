fe87dd976eeae0575d22182336ebabf002027aff
oscar.garcia <oscar.garcia@quipux.com>
2022-09-08 21:47:47 -0500

    feat: Sensores humedad y temperatura

--- app.py
+++ app.py
@@ -5,57 +5,59 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},}
+SENSOR = {
+    'sensor1': {'val': '10', 'tipo': 'temperatura'},
+    'sensor2': {'val': '60', 'tipo': 'humedad'},
+    'sensor3': {'val': '40', 'tipo': 'temperatura'},
+    'sensor4': {'val': '80', 'tipo': 'humedad'}}
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_sensor_doesnt_exist(sensor_id):
+    if sensor_id not in SENSOR:
+        abort(404, message=f"No existe {sensor_id}")
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('valor')
+parser.add_argument('tipo')
 
 
 # Todo
 # shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+class Sensor(Resource):
+    def get(self, sensor_id):
+        abort_if_sensor_doesnt_exist(sensor_id)
+        return SENSOR[sensor_id]
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
+    def delete(self, sensor_id):
+        abort_if_sensor_doesnt_exist(sensor_id)
+        del SENSOR[sensor_id]
         return '', 204
 
-    def put(self, todo_id):
+    def put(self, sensor_id):
         args = parser.parse_args()
         task = {'task': args['task']}
-        TODOS[todo_id] = task
+        SENSOR[sensor_id] = task
         return task, 201
 
 
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+# ListaSensores
+# shows a list of all SENSOR, and lets you POST to add new tasks
+class ListaSensores(Resource):
     def get(self):
-        return TODOS
+        return SENSOR
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        sensor_id = int(max(SENSOR.keys()).lstrip('todo')) + 1
+        sensor_id = 'todo%i' % sensor_id
+        SENSOR[sensor_id] = {'task': args['task']}
+        return SENSOR[sensor_id], 201
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(ListaSensores, '/sensores')
+api.add_resource(Sensor, '/sensores/<sensor_id>')
 
 
 if __name__ == '__main__':
